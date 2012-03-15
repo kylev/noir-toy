4c5c5e6ea37fae21bdeb38ca3df3aaf99fae3277
Kyle VanderBeek <kyle@change.org>
2012-03-14 17:25:58 -0700

    Base noir site:
    $ lein noir new noir-toy

---
+++ src/noir_toy/views/common.clj
@@ -0,0 +1,12 @@
+(ns noir-toy.views.common
+  (:use [noir.core :only [defpartial]]
+        [hiccup.page-helpers :only [include-css html5]]))
+
+(defpartial layout [& content]
+            (html5
+              [:head
+               [:title "noir-toy"]
+               (include-css "/css/reset.css")]
+              [:body
+               [:div#wrapper
+                content]]))
---
+++ src/noir_toy/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns noir-toy.views.welcome
+  (:require [noir-toy.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to noir-toy"]))
