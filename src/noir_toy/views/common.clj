da83ab958a646861c7901cb938c792d39e177b2e
Kyle VanderBeek <kyle@change.org>
2012-03-16 11:30:57 -0700

    I'm going to want some jquery at some point.

--- src/noir_toy/views/common.clj
+++ src/noir_toy/views/common.clj
@@ -1,11 +1,12 @@
 (ns noir-toy.views.common
   (:use [noir.core :only [defpartial]]
-        [hiccup.page-helpers :only [include-css html5]]))
+        [hiccup.page-helpers :only [include-css include-js html5]]))
 
 (defpartial layout [& content]
             (html5
               [:head
-               [:title "noir-toy"]
+               [:title "noir-toy!"]
+               (include-js "//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js")
                (include-css "/css/reset.css")]
               [:body
                [:div#wrapper
