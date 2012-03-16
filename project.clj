bfea20ff635dec2f3bf50c15ab296e3b56a7e58e
Kyle VanderBeek <kyle@change.org>
2012-03-16 11:30:39 -0700

    Add redis lib.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject noir-toy "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.2.1"]
+                           [org.clojars.strongh/clj-redis "0.1.0"]]
             :main noir-toy.server)
 
